e76c8002aeaacaed95a2ebb2599d02939d4619dc
anwalv <aoleksandrovych@kse.org.ua>
2024-03-20 12:16:57 +0200

    update some queries

--- .idea/pa3.sql
+++ .idea/pa3.sql
@@ -1,13 +1,13 @@
 /*-------SELECTS--------*/
     
 /*.1. виводить інформацію про автора у якого найдешевша книга(non corelated, =)*/
-SELECT author.name AS author_name, biography
-FROM author
-JOIN book_author ON author.id = book_author.author_id
-JOIN books ON book_author.book_id = books.id
+SELECT a.name AS author_name, biography
+FROM author a
+JOIN book_author ba ON a.id = ba.author_id
+JOIN books ON ba.book_id = books.id
 WHERE books.price = (
     SELECT MIN(price)
-    FROM books
+    FROM books 
 );
 
 
@@ -15,19 +15,19 @@ WHERE books.price = (
 SELECT b.title AS book_title, b.price AS book_price
 FROM books b
 WHERE b.price IN (
-    SELECT  price
-    FROM books
+    SELECT  b1.price
+    FROM books b1
     GROUP BY price
     HAVING COUNT(*) > 1
 );
 
 /*.3.вибирає авторів чиї книги ніколи не брали користувачі (non corelated, NOT IN)*/
-SELECT author.name AS author_name
-FROM author
-JOIN book_author ON author.id = book_author.author_id
-WHERE book_author.book_id NOT IN (
-    SELECT book_loan.book_id
-    FROM book_loan
+SELECT a.name AS author_name
+FROM author a
+JOIN book_author ba ON a.id = ba.author_id
+WHERE ba.book_id NOT IN (
+    SELECT bl.book_id
+    FROM book_loan bl
 );
 
 
@@ -40,19 +40,19 @@ WHERE b.id NOT IN (
     WHERE bl.loan_id IN (
         SELECT l.id 
         FROM loan l 
-        WHERE return_date IS NULL));
+        WHERE l.return_date IS NULL));
 
 
 /*.5.вибирає авторів в яких існують книги в жанрі горор(non corelated, EXISTS)*/
-SELECT author.name AS author_name
-FROM author
-JOIN book_author ON author.id = book_author.author_id
-JOIN books ON book_author.book_id = books.id
+SELECT a.name AS author_name
+FROM author a
+JOIN book_author ba ON a.id = ba.author_id
+JOIN books b ON ba.book_id = b.id
 WHERE EXISTS (
     SELECT 1
-    FROM book_genre
-    JOIN genres ON book_genre.genre_id = genres.id
-    WHERE book_genre.book_id = books.id AND genres.genre = 'Horror'
+    FROM book_genre bg
+    JOIN genres g ON bg.genre_id = g.id
+    WHERE g.genre = 'Horror'
 );
 
 /*.6.книги які не були взяті в позику(corelated, NOT EXISTS)*/
@@ -62,18 +62,17 @@ WHERE NOT EXISTS (
     SELECT 1
     FROM loan l
     JOIN book_loan bl ON l.id = bl.loan_id
-    WHERE bl.book_id = b.id
 );
 
 /*.7.вибирає авторів чиї книги написані не в жанрі drama (corelated, NOT IN)*/
 SELECT a.name AS author_name
 FROM author a
-         JOIN book_author ba ON a.id = ba.author_id
+JOIN book_author ba ON a.id = ba.author_id
 WHERE NOT EXISTS (
           SELECT 1
           FROM books b
-                   JOIN book_genre bg ON b.id = bg.book_id
-                   JOIN genres g ON bg.genre_id = g.id
+            JOIN book_genre bg ON b.id = bg.book_id
+            JOIN genres g ON bg.genre_id = g.id
           WHERE b.id = ba.book_id AND g.genre = 'Drama'
 );
 /*.8.вибирає інформацію про книги у яких заданий автор (corelated, =)*/ 
@@ -101,7 +100,6 @@ FROM books b
 WHERE EXISTS (
     SELECT 1
     FROM book_loan bl
-    JOIN books ON bl.book_id = b.id
     JOIN book_genre bg ON bg.book_id = b.id
     JOIN genres g ON bg.genre_id = g.id
     WHERE b.id = bl.book_id AND g.genre = 'Thriller'
@@ -117,8 +115,8 @@ WHERE EXISTS (
 UPDATE books b
 SET b.price = b.price * 1.1
 WHERE b.id NOT IN (
-    SELECT book_id
-    FROM book_loan
+    SELECT bl.book_id
+    FROM book_loan bl
 );
 
 /*.2. Збільшення ціни на 30% у книг які хоч раз були взяті в борг(correlated, EXISTS)*/
@@ -142,8 +140,8 @@ WHERE NOT EXISTS (
 
 /*.4.Оновлюється дата повернення книг, через закриття бібліотеки в Житомирі(non-correlated, IN)*/
 UPDATE loan l
-SET due_date = '2024-05-08'
-WHERE client_id IN (
+SET l.due_date = '2024-05-08'
+WHERE l.client_id IN (
     SELECT c.id
     FROM clients c
     WHERE c.address LIKE '%Zhytomyr%'
@@ -152,7 +150,7 @@ WHERE client_id IN (
 UPDATE books b
 SET b.price = b.price * 1.4
 WHERE b.publisher_id = (
-    SELECT id
+    SELECT p.id
     FROM publisher p
     WHERE p.name = 'Vivat'
 );
@@ -172,9 +170,9 @@ WHERE a.id IN (
     )
 );
 /*.7 оновлює рік публікації книг, які були видані певним видавцем якщо ці книги в позиках(не дуже make sense але фантація закінчилась)(non correlated, exists)*/
-UPDATE books
-SET publication_year = 2023
-WHERE publisher_id = 2
+UPDATE books b
+SET b.publication_year = 2023
+WHERE b.publisher_id = 2
   AND EXISTS (
     SELECT 1
     FROM book_author ba
@@ -186,32 +184,32 @@ WHERE publisher_id = 2
 UPDATE clients c
 SET c.name = CONCAT(c.name, ' [Inactive User]')
 WHERE c.id NOT IN (
-    SELECT DISTINCT client_id
+    SELECT l.client_id
     FROM loan l
     WHERE l.client_id = c.id
 );
 
-/*9.(correlated,=)*/
-UPDATE loan AS l
+/*9. Оновлює дату повернення книги на поточну дату для кредитів, які ще не повернуті і які прострочені(correlated,=)*/
+UPDATE loan l
 SET return_date = CURRENT_DATE
 WHERE l.return_date IS NULL
   AND l.due_date < CURRENT_DATE
   AND l.id = (
     SELECT bl.loan_id
     FROM book_loan AS bl
-             INNER JOIN books AS b ON bl.book_id = b.id
+    INNER JOIN books AS b ON bl.book_id = b.id
     WHERE bl.loan_id = l.id
 );
 
 /*.10.оновлює рік публікації книг на 2016, які були видані видавцем з id 4, і яких немає в позиках.(non-correlated, NOT EXIST)*/
-UPDATE books
-SET publication_year = 2016
-WHERE publisher_id = 4
+UPDATE books b 
+SET b.publication_year = 2016
+WHERE b.publisher_id = 4
   AND NOT EXISTS (
     SELECT 1
     FROM book_author ba
-             JOIN author a ON ba.author_id = a.id
-    WHERE ba.book_id = books.id
+    JOIN author a ON ba.author_id = a.id
+    WHERE ba.book_id = b.id
       AND a.nationality = 'America'
 );
 
@@ -220,87 +218,168 @@ WHERE publisher_id = 4
 /*--------DELETE-------/*
 
   
-/*.1.(non-correlated, =)*/
+/*.1.видаляємо ті книги де найвища ціна(non-correlated, =)*/
 DELETE FROM books
 WHERE price = (
     SELECT MAX(price)
     FROM books
 );
-/*.2.(non-correlated, IN)*/
+/*.2.Видаляємо книги з жанром горор(non-correlated, IN)*/
 DELETE FROM books
 WHERE genre_id IN (
     SELECT id
-    FROM genres
-    WHERE genre = 'Horror'
+    FROM genres g
+    WHERE g.genre = 'Horror'
 );
-/*.3.(non-correlated,NOT IN)*/
+/*.3.Видаляємо книги які за жанром не триллер(non-correlated,NOT IN)*/
 DELETE FROM books
 WHERE genre_id NOT IN (
     SELECT id
-    FROM genres
-    WHERE genre = 'Thriller'
+    FROM genres g
+    WHERE g.genre = 'Thriller'
 );
-/*(correlated,IN)*/
-DELETE FROM books
+/*.4. Видаляємо книги, які належать жанрам з менш ніж 3 книгами(correlated,IN)*/
+DELETE FROM books b
 WHERE genre_id IN (
     SELECT genre_id
     FROM book_genre
-    WHERE book_genre.book_id = books.id
+    WHERE book_id = b.id
+    GROUP BY genre_id
+    HAVING COUNT(*) < 3
 );
 
-/*(correlated, NOT IN)*/
-DELETE FROM books
-WHERE genre_id NOT IN (
-    SELECT genre_id
-    FROM book_genre
-    WHERE book_genre.book_id = books.id
+/*.5.Видаляємо книги, які мають більше одного автора(correlated, NOT IN)*/
+DELETE FROM books b 
+WHERE b.id IN (
+    SELECT ba.book_id
+    FROM book_author ba
+    WHERE ba.book_id = b.id
+    GROUP BY ba.book_id
+    HAVING COUNT(*) > 1
 );
-/*(correlated NOT EXIST)*/
 
-DELETE FROM books
+/*.6.Видаляє книги, які мають ціну вищу за середню ціну книг у їхньому жанрі.(correlated NOT EXIST)*/
+
+DELETE FROM books b1
 WHERE NOT EXISTS (
     SELECT 1
-    FROM book_genre
-    WHERE book_genre.book_id = books.id
-      AND book_genre.genre_id = 1 
+    FROM books b2
+    WHERE b1.genre_id = b2.genre_id
+    GROUP BY b2.genre_id
+    HAVING AVG(b2.price) < b1.price
 );
-/*(correlated,  EXIST)*/
-DELETE FROM loan
+
+/*.7.Видаляє записи,у яких книги були повернені(correlated,  EXIST)*/
+DELETE FROM loan l
 WHERE EXISTS (
     SELECT 1
-    FROM book_loan
-    WHERE book_loan.loan_id = loan.id
+    FROM book_loan bl
+    WHERE bl.loan_id = l.id
       AND EXISTS (
         SELECT 1
         FROM loan AS returned_loan
-        WHERE returned_loan.id = loan.id
-          AND returned_loan.return_date IS NOT NULL 
+        WHERE returned_loan.id = l.id
+        AND returned_loan.return_date IS NOT NULL 
     )
 );
-/*(non-correlated,  EXIST)*/
+/*.8.Видаляє книги які були взяті у позику у Льовові(non-correlated,  EXIST)*/
 DELETE FROM books
 WHERE EXISTS (
     SELECT 1
-    FROM loan
-    JOIN clients ON loan.client_id = clients.id
-    WHERE clients.address LIKE '%Львів%' 
+    FROM loan l
+    JOIN clients c ON l.client_id = c.id
+    WHERE c.address LIKE '%Львів%' 
 );
 
-/*(non-correlated,  NOT EXIST)*/
+/*.9.Видаляє клієнтів які не брали книги з жанру фентезі(бо як вони могли, не мають права бути тепер клієнтами)(non-correlated,  NOT EXIST)*/
 DELETE FROM clients
 WHERE NOT EXISTS (
     SELECT 1
-    FROM loan
-             JOIN book_genre ON loan.book_id = book_genre.book_id
-             JOIN genres ON book_genre.genre_id = genres.id
-    WHERE genres.genre = 'Fantasy'
+    FROM loan l 
+    JOIN book_genre bg ON l.book_id = bg.book_id
+    JOIN genres g ON bg.genre_id = g.id
+    WHERE g.genre = 'Fantasy'
 );
-/*(correlated, =)*/
-DELETE FROM books
+
+/*.10.Видаляє запис з книг, якщо хоча б один клієнт з іменем "Іван Петров" позичив цю книгу(вкрав напевно)(correlated, =)*/
+DELETE FROM books b
 WHERE EXISTS (
     SELECT 1
-    FROM loan AS l
-             JOIN clients AS c ON l.client_id = c.id
-    WHERE l.book_id = books.id
-      AND c.name = 'Ivan Petrov'
+    FROM loan l
+    JOIN clients c ON l.client_id = c.id
+    WHERE l.book_id = b.id
+    AND c.name = 'Ivan Petrov'
 );
+
+
+/*-----union-----*/
+
+/* Виводить книги з тих двох жанрів лише унікальні результати*/
+SELECT b.id, b.title
+FROM books b
+JOIN book_genre bg ON b.id = bg.book_id
+JOIN genres g ON bg.genre_id = g.id
+WHERE g.genre = 'Thriller' 
+
+UNION
+
+SELECT b.id, b.title
+FROM books b
+JOIN book_genre bg ON b.id = bg.book_id
+JOIN genres g ON bg.genre_id = g.id
+WHERE g.genre = 'Mystery' ;
+
+
+/*------union all------*/
+/* Авторів які є американцями або класиками, виводить дублікати*/
+
+SELECT a.id, a.name
+FROM author a
+WHERE a.nationality = 'America'
+
+UNION ALL
+
+SELECT a.id, a.name
+FROM author a
+JOIN book_author ba ON a.id = ba.author_id
+JOIN books b ON ba.book_id = b.id
+JOIN book_genre bg ON b.id = bg.book_id
+JOIN genres g ON bg.genre_id = g.id
+WHERE g.genre = 'Classic';
+
+
+
+/*-------INTERSECT------*/
+/*Результат буде включати тільки тих авторів, які мають книги, опубліковані як видавцями 'Vivat' і 'Folio' одночасно*/
+
+SELECT a.id, a.name
+FROM author a
+JOIN book_author ba ON a.id = ba.author_id
+JOIN books b ON ba.book_id = b.id
+JOIN publisher p ON b.publisher_id = p.id
+WHERE p.name = 'Vivat'
+
+INTERSECT
+
+SELECT a.id, a.name
+FROM author a
+JOIN book_author ba ON a.id = ba.author_id
+JOIN books b ON ba.book_id = b.id
+JOIN publisher p ON b.publisher_id = p.id
+WHERE p.name = 'Folio';
+
+/*--------EXCEPT-------*/
+/*Вибирає книги, які опубліковані видавцем з назвою 'Vivat', але при цьому виключає ті книги, які належать до жанру 'Fantasy'.*/
+
+SELECT b.title
+FROM books b
+JOIN publisher p ON b.publisher_id = p.id
+WHERE p.name = 'Vivat'
+
+EXCEPT
+
+SELECT b.title
+FROM books b
+JOIN book_genre bg ON b.id = bg.book_id
+JOIN genres g ON bg.genre_id = g.id
+WHERE g.genre = 'Fantasy';
